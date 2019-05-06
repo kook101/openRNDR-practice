3aa1274ac5b97b5c3fa3c4657d1e5e94a512d413
Jaekook Han <jaekook.h@gmail.com>
2019-05-06 18:45:32 -0400

    test drawing circles

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -4,6 +4,8 @@ import org.openrndr.color.mix
 import org.openrndr.draw.FontImageMap
 import org.openrndr.draw.loadImage
 import org.openrndr.draw.tint
+import org.openrndr.shape.Circle
+
 
 fun main() = application {
     configure {
@@ -15,6 +17,8 @@ fun main() = application {
         val image = loadImage("file:data/images/pm5544.png")
         val font = FontImageMap.fromUrl("file:data/fonts/IBMPlexMono-Regular.ttf", 64.0)
 
+        var x = 0.0;
+
         extend {
             drawer.drawStyle.colorMatrix = tint(ColorRGBa.WHITE.shade(0.2))
             drawer.image(image)
@@ -23,11 +27,22 @@ fun main() = application {
             drawer.circle(Math.cos(seconds)*width/2.0+width/2.0, Math.sin(0.5*seconds)*height/2.0 + height/2.0, 140.0)
 
 
+            val circles = List(500) {
+                Circle(Math.random() * width, Math.random() * height, Math.random() * 10.0 + 10.0)
+            }
+            drawer.circles(circles)
+
             val redCol = ColorRGBa.PINK
             val greenCol = ColorRGBa.GREEN
 
+            x += 10;
+
+            if(x > width){
+                x = 0.0;
+            }
+
             drawer.fill = mix(redCol, greenCol, (Math.cos(seconds) + 1) / 2.0);
-            drawer.rectangle(width / 2.0, height / 2.0, 50.0, 100.0);
+            drawer.rectangle(x, height / 2.0, 50.0, 100.0);
 
 
             drawer.fontMap = font
@@ -36,3 +51,10 @@ fun main() = application {
         }
     }
 }
+
+//class Particle{
+//
+//    constructor(x: Double, y : Double){
+//
+//    }
+//}
